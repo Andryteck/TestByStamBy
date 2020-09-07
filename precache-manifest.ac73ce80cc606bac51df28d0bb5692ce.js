e8e2c2066cb23c655e2337f5254fd92fa49e444c
Andryteck <772316andrew@gmail.com>
2020-09-07 19:35:19 +0300

    Updates

---
+++ precache-manifest.ac73ce80cc606bac51df28d0bb5692ce.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dab2cded745d03e165fafe710f7ceb",
+    "url": "/TestByStamBy/index.html"
+  },
+  {
+    "revision": "7c3206b8a3d79504d7b9",
+    "url": "/TestByStamBy/static/css/main.c074fd04.chunk.css"
+  },
+  {
+    "revision": "2bb4693985276efb9882",
+    "url": "/TestByStamBy/static/js/2.c9bce5a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TestByStamBy/static/js/2.c9bce5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3206b8a3d79504d7b9",
+    "url": "/TestByStamBy/static/js/main.547342c6.chunk.js"
+  },
+  {
+    "revision": "023ecd42e3a42ea356b5",
+    "url": "/TestByStamBy/static/js/runtime-main.9d4ec452.js"
+  },
+  {
+    "revision": "ea791be470d1e092362238efe425ea5d",
+    "url": "/TestByStamBy/static/media/cart-2.ea791be4.svg"
+  },
+  {
+    "revision": "57b7048589f5a08d359cc29c84abf722",
+    "url": "/TestByStamBy/static/media/favorite.57b70485.svg"
+  },
+  {
+    "revision": "73bb6c8075e92364027a239caa32fac0",
+    "url": "/TestByStamBy/static/media/imgProd3.73bb6c80.svg"
+  },
+  {
+    "revision": "bd9e81a4962f9d7e81262ed3d88799f4",
+    "url": "/TestByStamBy/static/media/product-image.bd9e81a4.svg"
+  },
+  {
+    "revision": "e187ff3ad10ffc17c653493da19752c5",
+    "url": "/TestByStamBy/static/media/separator.e187ff3a.svg"
+  },
+  {
+    "revision": "05ca64613b860ff68f17f764e9efdfc0",
+    "url": "/TestByStamBy/static/media/square.05ca6461.svg"
+  }
+]);
